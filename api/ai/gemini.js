06a17b4179b0dc1fd6c8a29d9d2ebf692656d623
SkyNest <haikalzy011@gmail.com>
2026-03-04 21:58:42 +0700

    Update gemini.js

--- api/ai/gemini.js
+++ api/ai/gemini.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
   const { GoogleGenerativeAI } = require("@google/generative-ai")
 
-  const genAI = new GoogleGenerativeAI("AIzaSyAlbK2NP8qM8vLzfJmtGSFE_z4dLADvYso")
+  const genAI = new GoogleGenerativeAI("AIzaSyA1hu0yaOTceWWVZtUypv1VVHXT7PmL2xc")
 
   async function Llama(prom) {
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
